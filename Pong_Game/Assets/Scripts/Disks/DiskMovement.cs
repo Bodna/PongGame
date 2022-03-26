bbc04b00540303b72b1fae24d10768f8296bd13e
Bodna <bodnarescu.lucian04@gmail.com>
2022-03-26 12:12:02 +0100

    Update on collision
    changed collisions on objects and setted script for velocity below 8 (apply random force) not set if velocity too high.

--- Pong_Game/Assets/Scripts/Disks/DiskMovement.cs
+++ Pong_Game/Assets/Scripts/Disks/DiskMovement.cs
@@ -6,12 +6,24 @@ public class DiskMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody rb;
+    private float high;
 
     private void Start() {
         rb = GetComponent<Rigidbody>();
         rb.AddForce(Vector3.forward * speed, ForceMode.Impulse);
+        high = transform.position.y;
     }
     private void Update() {
-        Debug.Log(rb.velocity);
+        //Debugging velocity
+        Debug.Log(Mathf.Abs(rb.velocity.x) + Mathf.Abs(rb.velocity.y) + Mathf.Abs(rb.velocity.z));
+        //Appplying force if elocity too low
+       
+        ApplyRandomForce();
+        transform.position = new Vector3(transform.position.x, high, transform.position.z);
+    }
+
+    public void ApplyRandomForce(){
+        if((Mathf.Abs(rb.velocity.x) + Mathf.Abs(rb.velocity.y) + Mathf.Abs(rb.velocity.z)) < 8)
+            rb.AddForce(new Vector3(Random.Range(5, 15), 0, Random.Range(5, 15)), ForceMode.Impulse);
     }
 }
